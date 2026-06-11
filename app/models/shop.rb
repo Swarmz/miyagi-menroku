26a7be57ee7e3d35bfed766b47c0cfbce717d349
Swarmz <gavinsutton6511@gmail.com>
2026-06-11 16:10:36 +0900

    Update shop and user models to include associations with visits

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,7 @@ class Shop < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :city, presence: true
   validates :address, presence: true
+
+  has_many :visits, dependent: :destroy
+  has_many :users, through: :visits
 end
